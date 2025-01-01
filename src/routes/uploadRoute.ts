4f4ed2f160cf64012e07f3bb87e94bf8aa1f8c61
thezoot3 <thezoot3@gmail.com>
2025-01-02 08:20:55 +0900

    totalDuration

--- src/routes/uploadRoute.ts
+++ src/routes/uploadRoute.ts
@@ -31,7 +31,6 @@ router.post('/', upload.single('video'), async (req, res) => {
     }
 
     const uploadEndTime = Date.now();
-    const uploadDuration = (uploadEndTime - req.body.uploadStartTime) / 1000; // 초 단위로 변환
 
     // 파일 크기 확인
     const stats = fs.statSync(req.file.path);
@@ -41,8 +40,19 @@ router.post('/', upload.single('video'), async (req, res) => {
     console.log(`File uploaded successfully: ${req.file.originalname}`);
     console.log(`File size: ${fileSizeInMegabytes.toFixed(2)} MB`);
     console.log(`Upload duration: ${uploadDuration.toFixed(2)} seconds`);
-    res.send('File uploaded successfully');
-    await saveVideoInfo(req.file);
+
+    try {
+        const totalDuration = (Date.now() - req.body.uploadStartTime) / 1000;
+        const videoInfo = await saveVideoInfo(req.file);
+        res.json({
+            ...videoInfo,
+            fileSize: `${fileSizeInMegabytes.toFixed(2)} MB`,
+            uploadDuration: `${totalDuration.toFixed(2)} seconds`
+        });
+    } catch (error) {
+        console.error('Error processing video:', error);
+        res.status(500).send('Error processing video.');
+    }
 });
 
 
